737d2e705d01f040703a0ebedb3e22f4feef431a
Alina <alinarosi169@gmail.com>
2026-03-19 11:14:57 +0100

    sistemati problemi relativi a conflitti

--- backend/src/main/java/app/web/mobile/cloud/my_study_planner/dto/AuthResponse.java
+++ backend/src/main/java/app/web/mobile/cloud/my_study_planner/dto/AuthResponse.java
@@ -1,24 +1,11 @@
 package app.web.mobile.cloud.my_study_planner.dto;
 
-<<<<<<< HEAD
-/**
- * DTO (Data Transfer Object) per la restituzione dei dati di autenticazione al client
- * 
- * Contiene le informazioni principali dell'utente autenticato insieme
- * al token JWT necessario per autorizzare le richieste successive
- */
-=======
->>>>>>> 60275efaa897e2ccda2fb5d032e748394b97ad35
 public class AuthResponse {
 
     private Long id;
     private String username;
     private String email;
-<<<<<<< HEAD
     private String token; 
-=======
-    private String token; // <- nuovo
->>>>>>> 60275efaa897e2ccda2fb5d032e748394b97ad35
 
     public AuthResponse(Long id, String username, String email, String token) {
         this.id = id;
--- backend/src/main/java/app/web/mobile/cloud/my_study_planner/dto/RegisterRequest.java
+++ backend/src/main/java/app/web/mobile/cloud/my_study_planner/dto/RegisterRequest.java
@@ -1,16 +1,5 @@
 package app.web.mobile.cloud.my_study_planner.dto;
 
-<<<<<<< HEAD
-/**
- * DTO utilizzato per ricevere i dati di registrazione
- * di un nuovo utente dal client.
- *
- * Contiene le informazioni necessarie alla creazione di un account
- * all'interno del sistema.
- */
-
-=======
->>>>>>> 60275efaa897e2ccda2fb5d032e748394b97ad35
 public class RegisterRequest {
 
     private String username;
